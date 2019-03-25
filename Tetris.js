bf30c4728bc853cf741bc840b8873dc771d39703
Leon Ebert <47148350+Veromas2k@users.noreply.github.com>
2019-03-25 17:00:38 +0100

    Add files via upload

--- Tetris.js
+++ Tetris.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){	
 //###################################
-//								constants
+//constants
 //###################################
 
 	const canvas = document.getElementById("canvas");
@@ -8,44 +8,117 @@ $(document).ready(function(){
 	canvas.height = 800;
 	canvas.width = 400;
 	ctx.scale(20,20);
-	ctx.fillStyle = "black";
+	const arena = createMatrix(20,40);
+	console.table(arena);
 
 	
 
 	
 //###################################
-//								Variables
+//Variables
 //###################################
-
-	var matrix = [
+	var dropCounter = 0;
+	var dropInterval = 1000; //ms
+	var lastTime = 0;
+	var stop = false;
+	var matrix 
+	= [{blocks: [// T = 0
 		[0, 0, 0],
 		[1, 1, 1,],
 		[0, 1, 0],
+	],color: "purple"},
+	{blocks: [//J = 1
+		[0, 0, 0],
+		[1, 0, 0],
+		[1, 1, 1],
+	], color: "blue"},
+	{blocks: [//L = 2
+		[0, 0, 0],
+		[0, 0, 1],
+		[1, 1, 1,],
+	], color: "orange"},
+	
 	];
 	
-	function drawMatrix(matrix){
+	var piece = {
+		pos: {x: 9, y: 0},
+		matrix: matrix[2].blocks,
+		color: matrix[2].color,
+	}
+	
+//###################################
+//functions
+//###################################
+
+	function createMatrix(w,h){
+		const matrix = [];
+		while (h--){
+			matrix.push(new Array(w).fill(0));
+		}
+		return matrix;
+	}
+	
+	function drawMatrix(matrix,px,py,color){
 		matrix.forEach((row, y) => {
 			row.forEach((value, x) => {
 				if (value !== 0) {
-					ctx.fillStyle ="purple";
-					ctx.fillRect(x, y, 1, 1);
+					ctx.fillStyle =color;
+					ctx.fillRect(px + x,  py + y, 1, 1);
 					ctx.fillStyle = "black";
 					ctx.lineWidth = 0.05;
-					ctx.strokeRect(x, y, 1, 1);
+					ctx.strokeRect(px + x,py + y, 1, 1);
 				}
 			});
 		});
 	}
 	
-	drawMatrix(matrix);
-//####################################
-//									game
-//####################################
+	function checkDrop(matrix){
+		matrix.forEach((row, y) => {
+			row.forEach((value, x) => {
+				if(value == 1){
+					if((piece.pos.y + y)+1 == 40){
+						stop = true;
+					}
+				}
+			});
+		});
+	}
+	
+	function draw(){
+		ctx.clearRect(0,0,40,80);
+		drawMatrix(piece.matrix,piece.pos.x,piece.pos.y,piece.color);
+	}
+	
+	function pieceDrop(){
+		piece.pos.y++;
+		dropCounter = 0;
+	}
+	
+	function update(time = 0){
+		const deltaTime = time - lastTime;
+		lastTime = time;
+		dropCounter += deltaTime;
+		if (dropCounter > dropInterval){
+			checkDrop(piece.matrix);
+			if(stop == true){
+				//alert("!");
+				stop = false;
+			}else{
+				pieceDrop();
+			}
+		}
 
+		draw();
+		requestAnimationFrame(update);
+	}
+	
+	
+//####################################
+//game
+//####################################
+	
+	update();
 
-/* 	ctx.fillStyle = "black";
-	ctx.lineWidth = 0.1;
-	ctx.strokeRect(6,6,4,2); */
 
 //#####################################
 //controls
@@ -54,18 +127,27 @@ $(document).ready(function(){
 		switch(event.keyCode){
 			case 32://space
 				
-			break;
+				break;
 			case 38://up
-
+				dropCounter =0;
+				draw();
 				break;
 			case 40://down
-	
+				checkDrop(piece.matrix);
+				if(stop == false){
+					pieceDrop();
+				}
+					draw();
 				break;
 			case 39://right
-			
+				piece.pos.x ++;
+				dropCounter = 0;
+				draw();			
 				break;
 			case 37://left
-			
+				piece.pos.x --;
+				dropCounter = 0;
+				draw();			
 				break;
 		}
 	} 
