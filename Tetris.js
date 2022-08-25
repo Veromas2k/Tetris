4bc16cffad33d022b03fa8a403fb7654211d2696
Leon Ebert <47148350+Veromas2k@users.noreply.github.com>
2022-08-25 18:24:04 +0200

    New code: new bugs
    not the final product

--- Tetris.js
+++ Tetris.js
@@ -1,141 +1,84 @@
 $(document).ready(function(){	
+
 //###################################
-//constants
+//variables
 //###################################
 
-	const canvas = document.getElementById("canvas");
-	const ctx = canvas.getContext("2d");
-	canvas.height = 800;
-	canvas.width = 400;
-	ctx.scale(33.3333,32);
+	var gameCanvas = document.getElementById("gameCanvas");
+	var ctxG = gameCanvas.getContext("2d");		
+	ctxG.scale(33.3333,32);
+	var previewCanvas = document.getElementById("previewCanvas");
+	var ctxP = previewCanvas.getContext("2d");
 	var arena;
-	var colorBlock;
-	console.table(arena);
-
-	
-
-	
-//###################################
-//Variables
-//###################################
+	var maxHeight = 25;
+	var maxWidth = 12;
 	var dropCounter = 0;
 	var dropInterval = 1000; //ms
 	var lastTime = 0;
 	var stop = false;
-	var leftStop = false;
-	var rightStop = false;
-	var piece;
-	var rowVal = 0;
+	var currentPiece;
 	var dir = 1;
 	
-	var matrix 
-	= [{blocks: [// T = 0
-		[0, 1, 0],
-		[1, 1, 1,],
-		[0, 0, 0],
-	],color: "purple"},
-	{blocks: [//J = 1
-		[1, 1, 1],
-		[0, 0, 1],
-		[0, 0, 0],
-	], color: "blue"},
-	{blocks: [//L = 2
-		[1, 1, 1],
-		[1, 0, 0],
-		[0, 0, 0],
-	], color: "orange"},
-	{blocks: [// Z = 3
-		[1, 1, 0],
-		[0, 1, 1],
-		[0, 0, 0],
-	], color: "red"},
-	{blocks: [// S = 4
-		[0, 1, 1],
-		[1, 1, 0],
-		[0, 0, 0],
-	], color: "green"},
-	{blocks: [// O = 5
-		[1, 1],
-		[1, 1],
-	], color: "yellow"},
-	{blocks: [// I = 6
-		[0, 1, 0, 0],
-		[0, 1, 0, 0],
-		[0, 1, 0, 0],
-		[0, 1, 0, 0],
-	], color: "DeepSkyBlue"},
+	var piece = [
+		{matrix: [// T = 0
+			[0, 1, 0],
+			[1, 1, 1,],
+			[0, 0, 0],
+		],color: "purple"},
+		{matrix: [//J = 1
+			[1, 1, 1],
+			[0, 0, 1],
+			[0, 0, 0],
+		], color: "blue"},
+		{matrix: [//L = 2
+			[1, 1, 1],
+			[1, 0, 0],
+			[0, 0, 0],
+		], color: "orange"},
+		{matrix: [// Z = 3
+			[1, 1, 0],
+			[0, 1, 1],
+			[0, 0, 0],
+		], color: "red"},
+		{matrix: [// S = 4
+			[0, 1, 1],
+			[1, 1, 0],
+			[0, 0, 0],
+		], color: "green"},
+		{matrix: [// O = 5
+			[1, 1],
+			[1, 1],
+		], color: "yellow"},
+		{matrix: [// I = 6
+			[0, 1, 0, 0],
+			[0, 1, 0, 0],
+			[0, 1, 0, 0],
+			[0, 1, 0, 0],
+		], color: "DeepSkyBlue"},
 	];
 
 //###################################
 //functions
 //###################################
 
-	function pickPiece(number){
-		piece = {
-			pos: {x: 5, y: 0},
-			matrix: matrix[number].blocks,
-			color: matrix[number].color,
-		};
-	}
-	
 	function createMatrix(w,h){
 		const matrix = [];
 		while (h--){
-			matrix.push(new Array(w).fill(0));
+			matrix.push(new Array(w).fill(7));
 		}
 		return matrix;
 	}
-	
-	function drawArena(matrix){
-		matrix.forEach((row, y) => {
-			row.forEach((value, x) => {
-				if (value !== 0) {	
-					ctx.fillStyle = colorBlock[y][x];
-					ctx.fillRect(x, y, 1, 1);
-					ctx.fillStyle = "black";
-					ctx.lineWidth = 0.08;
-					ctx.strokeRect(x, y, 1, 1);
-				}
-			});
-		});
-	}	
+
+	function pickPiece(number){
+		currentPiece = {
+			pos: {x: 5, y: 0},
+			matrix: piece[number].matrix
+		};
+	}
 	
 	function rowClear(matrix){
-		matrix.forEach((row,y) =>{
-			row.forEach((value, x) =>{
-				if(value == 1){
-					rowVal++;
-				}
-			});
-			if(rowVal == 12){
-				//alert("12");
-				alert(matrix[y]);
-				let rowCount = 0;
-				while(rowCount < 25){
-					//alert(matrix[]);
-					matrix[y - rowCount] = matrix[y - (rowCount+1)];
-					
-					rowCount++;
-				}
-			}
-			rowVal = 0;
-		});
 	}
-	
-	function drawMatrix(matrix,px,py,color){
-		matrix.forEach((row, y) => {
-			row.forEach((value, x) => {
-				if (value !== 0) {
-					ctx.fillStyle =color;
-					ctx.fillRect(px + x,  py + y, 1, 1);
-					ctx.fillStyle = "black";
-					ctx.lineWidth = 0.08;
-					ctx.strokeRect(px + x,py + y, 1, 1);
-				}
-			});
-		});
-	}
-	
+
 	function rotate(matrix,dir){
 		for (let y = 0; y < matrix.length; ++y){
 			for (let x = 0; x < y; ++x){
@@ -148,7 +91,6 @@ $(document).ready(function(){
 				];
 			}
 		} 
-		//alert([matrix]);
 		if(dir > 0){
 			matrix.forEach(row => row.reverse());
 		}else{
@@ -159,73 +101,40 @@ $(document).ready(function(){
 	function checkDrop(matrix){
 		matrix.forEach((row, y) => {
 			row.forEach((value, x) => {
-				if(value == 1){
-					if((piece.pos.y + y)+1 == 25){
+				if(value !== 7){
+					if((currentPiece.pos.y + y)+1 == maxHeight){
 						stop = true;
-					}else if(arena[y + (piece.pos.y) + 1][x + (piece.pos.x)] == 1){
+					}else if(arena[y + (currentPiece.pos.y) + 1][x + (currentPiece.pos.x)] !== 7){
 						stop = true;
 					}
 				}
 			});
 		});
 	}
-	
-	function checkLeft(matrix){
-		matrix.forEach((row, y) => {
-			row.forEach((value, x) => {
-				if(value == 1){
-					if((piece.pos.x + x)-1 == -1 | arena[y + piece.pos.y][x + (piece.pos.x) - 1] == 1){
-						leftStop = true;
-					}
-				}
-			});
-		});
-	}
-	
-	function checkRight(matrix){
-		matrix.forEach((row, y) => {
-			row.forEach((value, x) => {
-				if(value == 1){
-					if((piece.pos.x + x)+1 == 12 | arena[y + piece.pos.y][x + (piece.pos.x)+1] == 1){
-						rightStop = true;
-					}
-				}
-			});
-		});
-	}
-	
-	function draw(){
-		ctx.clearRect(0,0,12,25);
-		drawArena(arena);
-		drawMatrix(piece.matrix,piece.pos.x,piece.pos.y,piece.color);
 		
-	}
-	
 	function pieceDrop(){
-		piece.pos.y++;
+		currentPiece.pos.y++;
 		dropCounter = 0;
 	}
 	
-	function mergeArenaPiece(arena,px,py,piece,color){
-		piece.matrix.forEach((row, y) => {
+	function mergeArenaPiece(arena,px,py,piece){
+		currentPiece.matrix.forEach((row, y) => {
 			row.forEach((value, x) => {
-				if(value == 1){
+				if(value !== 7){
 					arena[y + py][x + px] = value;
-					colorBlock[y + py][x + px] = color;
 				}
 			});
 		});		
 	}	
 	
-	alert(arena);
 	function update(time = 0){
 		const deltaTime = time - lastTime;
 		lastTime = time;
 		dropCounter += deltaTime;
 		if (dropCounter > dropInterval){
-			checkDrop(piece.matrix);
+			checkDrop(currentPiece.matrix);
 			if(stop == true){
-				mergeArenaPiece(arena,piece.pos.x,piece.pos.y,piece,piece.color);
+				mergeArenaPiece(arena,currentPiece.pos.x,currentPiece.pos.y,currentPiece);
 				pickPiece(Math.floor(Math.random() * (7)));
 				stop = false;
 			}else{
@@ -234,22 +143,80 @@ $(document).ready(function(){
 		rowClear(arena);
 		}
 		draw();
-		if(arena[2][6] == 1){
-			alert("!!");
+		if(arena[2][6] !== 7){
+			alert("GAME OVER");
 			start.style.display = "block";
 		}else{
 			requestAnimationFrame(update);
 		}
 	}
 	
+	function draw(){
+		ctxG.clearRect(0,0,maxWidth,maxHeight);
+		drawArena(arena);
+		drawMatrix(currentPiece.matrix,currentPiece.pos.x,currentPiece.pos.y);
+	}
 	
+	function drawArena(field){
+		field.forEach((row, y) => {
+			row.forEach((value, x) => {
+				if (value !== 7) {	
+					getFillstyleColor(value);
+					ctxG.fillRect(x, y, 1, 1);
+					ctxG.fillStyle = "black";
+					ctxG.lineWidth = 0.08;
+					ctxG.strokeRect(x, y, 1, 1);
+				}
+			});
+		});
+	}	
+	
+	function drawMatrix(matrix,px,py,color){
+		matrix.forEach((row, y) => {
+			row.forEach((value, x) => {
+				if (value !== 7) {
+					getFillstyleColor(value);
+					ctxG.fillRect(px + x,  py + y, 1, 1);
+					ctxG.fillStyle = "black";
+					ctxG.lineWidth = 0.08;
+					ctxG.strokeRect(px + x,py + y, 1, 1);
+				}
+			});
+		});
+	}
+	
+	function getFillstyleColor(value){
+		switch(value){
+			case 0:
+				ctxG.fillStyle = piece[0].color;
+				break;
+			case 1:
+				ctxG.fillStyle = piece[1].color;
+				break;
+			case 2:
+				ctxG.fillStyle = piece[2].color;
+				break;
+			case 3:
+				ctxG.fillStyle = piece[3].color;
+				break;
+			case 4:
+				ctxG.fillStyle = piece[4].color;
+				break;
+			case 5:
+				ctxG.fillStyle = piece[5].color;
+				break;						
+			case 6:
+				ctxG.fillStyle = piece[6].color;
+				break;
+		}
+	}
 //####################################
 //game
 //####################################
+
 start.addEventListener("click",startScript);
 function startScript(){
-	arena = createMatrix(12,25);
-	colorBlock = createMatrix(12,25);
+	arena = createMatrix(maxWidth,maxHeight);
 	start.style.display = "none";
 	pickPiece(Math.floor(Math.random() * (7)));
 	update();
@@ -258,11 +225,11 @@ function startScript(){
 //#####################################
 //controls
 //#####################################
+
  	window.onkeydown = function(event) {
 		switch(event.keyCode){
 			case 32://space
-				//pickPiece(Math.floor(Math.random() * (7)));
-				//alert(arena[2][6]);
+				//DEBUG VISUALS
 				console.table(arena);
 				break;
 			case 38://up
@@ -270,37 +237,26 @@ function startScript(){
 				if(dir>3){
 					dir = 1;
 				}
-				rotate(piece.matrix,dir);
+				rotate(currentPiece.matrix,dir);
 				draw();
 				break;
 			case 40://down
-				checkDrop(piece.matrix);
+				checkDrop(currentPiece.matrix);
 				if(stop == false){
 					pieceDrop();
 				}
 				if(stop == true){
-					mergeArenaPiece(arena,piece.pos.x,piece.pos.y,piece,piece.color);
+					mergeArenaPiece(arena,currentPiece.pos.x,currentPiece.pos.y,currentPiece);
 					stop = false;
 				}
-					//draw();
 				break;
 			case 39://right
-				checkRight(piece.matrix);
-				if(rightStop == false){
-					piece.pos.x ++;
-				}
-				rightStop = false;
-				dropCounter = dropCounter - 22;
-				//draw();			
+				currentPiece.pos.x ++;
+				dropCounter = dropCounter - 22;		
 				break;
 			case 37://left
-				checkLeft(piece.matrix);
-				if(leftStop == false){
-					piece.pos.x --;
-				}
-				leftStop = false;
-				dropCounter = dropCounter - 22;
-				//draw();			
+				currentPiece.pos.x --;
+				dropCounter = dropCounter - 22;		
 				break;
 		}
 	} 
